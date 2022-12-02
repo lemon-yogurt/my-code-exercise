67a578cf1f4220222a22862adbade3ae8d3979a5
qiuli <qiuli@qunhemail.com>
2022-12-02 15:16:04 +0800

    React开发环境配置

---
+++ src/index.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+ReactDom.render(<div>jsx环境配置成功！</div>, document.querySelector('#app'))
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    mode: 'development',
+    devServer: {
+        hot: true,
+        open: true
+    },
+    entry: "./src/index.jsx",
+    output: {
+        filename: "[name].js",
+        path: path.join(__dirname, "./dist"),
+    },
+    module: {
+        rules: [{
+        test: /\.jsx$/,
+        loader: "babel-loader",
+        options: {
+            presets: ["@babel/preset-react"],
+        }
+        }]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            templateContent: `
+                <!DOCTYPE html>
+                <html>
+                <head>
+                    <meta charset="utf-8">
+                    <title>Webpack App</title>
+                </head>
+                <body>
+                    <div id="app" />
+                </body>
+                </html>
+            `,
+        }),
+    ],
+};
